48a01d6e41453f4bea3c3fdd6db4234a3ba855cf
jinro1994 <w.chop.pp15@gmail.com>
2026-09-22 13:48:56 +0900

    Add Takarazuka data source file

---
+++ data.js
@@ -0,0 +1,23 @@
+// 宝塚人事データベース
+// 実データはこの配列に追加します。
+// 不明な項目は空文字 "" または 0 のままにします。
+
+window.TAKARAZUKA_DATA = [
+  /*
+  {
+    name: "芸名",
+    troupe: "花組",
+    role: "男役",
+    classYear: "100期",
+    status: "在団",
+    position: "",
+    shinko: 0,
+    bow: 0,
+    tojo: 0,
+    lineHeroine: 0,
+    wing: "",
+    memo: "",
+    updated: "2026-09-22"
+  }
+  */
+];
